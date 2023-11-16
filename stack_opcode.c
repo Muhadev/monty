c67b8480d3630f4c228c3de615cd370e7590822b
Muhadev <muhammedfayemi@gmail.com>
2023-11-16 06:51:55 -0800

    stack_opcode.c is added

---
+++ stack_opcode.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * stack_opcode - print the top of the stack
+ * @stack_head: stack head top
+ * @check: numbers of line occupied
+ * Return: no return
+ */
+void stack_opcode(stack_t **stack_head, unsigned int check)
+{
+	(void) check;
+	(void) stack_head;
+	pack.value = 0;
+}
