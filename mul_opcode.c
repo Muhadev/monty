d375e3ab959053ecb6ecc5e1c4630294fdffb44a
Muhadev <muhammedfayemi@gmail.com>
2023-11-16 05:21:21 -0800

    mul_opcode.c is added

---
+++ mul_opcode.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * mul_opcode - divide the first two element of the stack
+ * @stack_head: stack head top
+ * @check: numbers of line occupied
+ * Return: no return
+ */
+void mul_opcode(stact_t **stack_head, unsigned int check)
+{
+	stact_t *num;
+	int size = 0, temp;
+
+	num = *stack_head;
+	while (num)
+	{
+		num = num->next;
+		size++;
+	}
+	if (size < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", check);
+		fclose(pack.folder);
+		free(pack.info);
+		stack_empty(*stack_head);
+		exit(EXIT_FAILURE);
+	}
+	num = *stack_head;
+	temp = num->next->n * num->n;
+	num->next->n = temp;
+	*stack_head = num->next;
+	free(num);
+}
