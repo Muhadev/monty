5c7814582de74684735dedf0fc6540d3f2459b88
Muhadev <muhammedfayemi@gmail.com>
2023-11-16 06:12:35 -0800

    nop.c is added

--- nop.c
+++ nop.c
@@ -5,7 +5,7 @@
  * @check: numbers of line occupied
  * Return: no return
  */
-void nop_opcode(stact_t **stack_head, unsigned int check)
+void nop_opcode(stack_t **stack_head, unsigned int check)
 {
 	(void) check;
 	(void) stack_head;
