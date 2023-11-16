d2d178401512fc3f9c9e7d7f74cc264aa262bfe1
Muhadev <muhammedfayemi@gmail.com>
2023-11-16 06:58:11 -0800

    sub_opcode.c is added

---
+++ sub_opcode.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * sub_opcode - sub the first two element on the top of the stack
+ * @stack_head: the head of the stack
+ * @check:the numbers of line occupied
+ * Return: no return
+ */
+void sub_opcode(stack_t **stack_head, unsigned int check)
+{
+	stack_t *temp;
+	int size, num;
+
+	temp = *stack_head;
+
+	for (num = 0; temp != NULL; num++)
+	{
+		temp = temp->next;
+	}
+	if (num < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", check);
+		fclose(pack.folder);
+		free(pack.info);
+		stack_empty(*stack_head);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack_head;
+	sub = temp->next->n - temp->n;
+	temp->next->n = sub;
+	*stack_head = temp->next;
+	free(temp);
+}
